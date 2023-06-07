cb3eb0d655ee18f9c49b18269dd97290aff0d017
Avishay <avishaypesk@gmail.com>
2023-06-07 18:59:31 +0300

    cors

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const app = express();
 
 mongoose.connect(MONGODB_URL);
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
 app.use(express.json());
 app.use(helmet());
